f719b1273b6f5941e8066a148c213e6b047975e8
Maksyutka <maksyutka4579@gmail.com>
2022-10-13 02:17:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,80 @@
+$(".preheader__banner_pink").hide();
+setInterval(() => $(".preheader__banner").fadeToggle(500), 3000);
+
+function accordeonInit() {
+	$(".catalog__side-acc-content").hide();
+
+	$(".catalog__side-acc-head").click(function () {
+		$(this).parent().find(".catalog__side-acc-content").slideToggle(400);
+		$(this).parent().find(".catalog__side-acc-head").toggleClass("active");
+	});
+}
+
+function mobileMenuInit() {
+	$(".catalog__filters").click(function () {
+		$(".catalog__side").addClass("active");
+	});
+
+	$(".catalog__side-close").click(function () {
+		$(".catalog__side").removeClass("active");
+	});
+}
+
+function langDropdownInit() {
+	$(".header__lang-dropdown").hide();
+
+	$(".header__current-lang").click(function () {
+		$(".header__lang-dropdown").slideToggle();
+		$(".header__current-lang").toggleClass("active");
+	});
+}
+
+function sortDropdownInit() {
+	$(".catalog__sort-dropdown").hide();
+
+	$(".catalog__sort-choose").click(function () {
+		$(".catalog__sort-dropdown").slideToggle();
+		$(".catalog__sort-choose").toggleClass("active");
+	});
+}
+
+function sliderInit() {
+	$("#slider").slider({
+		animate: "slow",
+		range: "min",
+		value: 50,
+		min: 980,
+		max: 20465,
+	});
+
+	$("#slider").slider({
+		animate: "slow",
+		range: true,
+		values: [980, 20465],
+		slide: function (event, ui) {
+			$("#slider-min").val(ui.values[0]);
+			$("#slider-max").val(ui.values[1]);
+		},
+	});
+
+	$("#slider-min, #slider-max").keypress(function () {
+		if (event.keyCode == 13 && parseInt($("#slider-min").val()) < parseInt($("#slider-max").val())) {
+			$("#slider").slider("values", 0, $("#slider-min").val());
+			$("#slider").slider("values", 1, $("#slider-max").val());
+		}
+	});
+
+	$("#slider-min, #slider-max").blur(function () {
+		if (parseInt($("#slider-min").val()) < parseInt($("#slider-max").val())) {
+			$("#slider").slider("values", 0, $("#slider-min").val());
+			$("#slider").slider("values", 1, $("#slider-max").val());
+		}
+	});
+
+	$(".numberonly").keypress(function (e) {
+		var charCode = e.which ? e.which : event.keyCode;
+		if (String.fromCharCode(charCode).match(/[^0-9]/g)) return false;
+	});
+}
+
+$(document).ready(accordeonInit(), mobileMenuInit(), langDropdownInit(), sortDropdownInit(), sliderInit());
